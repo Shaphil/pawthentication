332d7901395e5fd011aec6bf763f54a184b223d7
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-10-25 17:55:25 +0600

    Add RegistrationView and its url mapping

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from accounts.views import UserProfile
+from accounts.views import UserProfile, RegistrationView
 
 
 urlpatterns = [
     path('profile/', UserProfile.as_view(), name='profile'),
+    path('register/', RegistrationView.as_view(), name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,13 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
 
 class UserProfile(TemplateView):
     template_name = 'accounts/profile.html'
+
+
+class RegistrationView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
